25eb98b9fa97ec8bdf78d4632dc15edebd9eb815
Kevin Watts <kevinwatts@replicant.com>
2020-12-14 21:10:36 -0800

    Clang format for sorting tests

--- sorting/test/test_sorting.cpp
+++ sorting/test/test_sorting.cpp
@@ -6,8 +6,8 @@
 #include "radix_sort.hpp"
 #include "testing_common.hpp"
 
-#include <algorithm> // std::sort, std::is_sorted
-#include <random>    // generate random input data
+#include <algorithm>  // std::sort, std::is_sorted
+#include <random>     // generate random input data
 
 #include <iostream>
 
@@ -17,105 +17,91 @@
 #include <boost/test/unit_test.hpp>
 
 // Functions can't be passed directly as template parameters
-template<typename IteratorT>
-struct HeapSortFunctorWrapper
-{
-    void operator()(IteratorT start, IteratorT end) const
-    {
-        return sorting_practice::heapSort(start, end);
-    }
+template <typename IteratorT>
+struct HeapSortFunctorWrapper {
+  void operator()(IteratorT start, IteratorT end) const {
+    return sorting_practice::heapSort(start, end);
+  }
 };
 
-template<typename IteratorT>
-struct RadixSortFunctorWrapper
-{
-    void operator()(IteratorT start, IteratorT end) const
-    {
-        return sorting_practice::radixSort(start, end);
-    }
+template <typename IteratorT>
+struct RadixSortFunctorWrapper {
+  void operator()(IteratorT start, IteratorT end) const {
+    return sorting_practice::radixSort(start, end);
+  }
 };
 
 /*
  * Test both radix and heap sort for speed and correctness
  */
-BOOST_AUTO_TEST_CASE(SortingPerformance)
-{
-    static const size_t LARGE_VEC = 1000000;
+BOOST_AUTO_TEST_CASE(SortingPerformance) {
+  static const size_t LARGE_VEC = 1000000;
 
-    typedef uint32_t UnsignedIntT;
+  typedef uint32_t UnsignedIntT;
 
-    std::vector<UnsignedIntT> elements;
-    sorting_practice::testing::createRandomVector<UnsignedIntT>(elements, 10000, LARGE_VEC);
+  std::vector<UnsignedIntT> elements;
+  sorting_practice::testing::createRandomVector<UnsignedIntT>(elements, 10000,
+                                                              LARGE_VEC);
 
-    UnsignedIntT* start = &elements.data()[0];
-    UnsignedIntT* end   = start + elements.size();
+  UnsignedIntT* start = &elements.data()[0];
+  UnsignedIntT* end = start + elements.size();
 
-    // radix
-    BOOST_REQUIRE(sorting_practice::testing::testPerformance(start, end,
-            RadixSortFunctorWrapper<UnsignedIntT*>(),
-            "radix_sort"));
+  // radix
+  BOOST_REQUIRE(sorting_practice::testing::testPerformance(
+      start, end, RadixSortFunctorWrapper<UnsignedIntT*>(), "radix_sort"));
 
-    // heap
-    BOOST_REQUIRE(sorting_practice::testing::testPerformance(start, end,
-            HeapSortFunctorWrapper<UnsignedIntT*>(),
-            "heap_sort"));
+  // heap
+  BOOST_REQUIRE(sorting_practice::testing::testPerformance(
+      start, end, HeapSortFunctorWrapper<UnsignedIntT*>(), "heap_sort"));
 }
 
 /*
  * Radix sorting should support custom types, with unsigned integer keys
  */
-struct MyStruct
-{
-    uint32_t key;
-    std::string dummy_value;
+struct MyStruct {
+  uint32_t key;
+  std::string dummy_value;
 };
 
-namespace sorting_practice
-{
+namespace sorting_practice {
 
-template<>
-struct RadixValueTraits<MyStruct>
-{
-    typedef uint32_t RadixKeyT;
+template <>
+struct RadixValueTraits<MyStruct> {
+  typedef uint32_t RadixKeyT;
 
-    static RadixKeyT getKey(MyStruct const &v)
-    {
-        return v.key;
-    }
+  static RadixKeyT getKey(MyStruct const& v) { return v.key; }
 };
 
-} // sorting_practice
+}  // sorting_practice
 
 // Define this, so std::sort works
-bool operator<(const MyStruct& k1, const MyStruct& k2)
-{
-    return k1.key < k2.key;
+bool operator<(const MyStruct& k1, const MyStruct& k2) {
+  return k1.key < k2.key;
 }
 
 // Verify radix sort works for custom type
-BOOST_AUTO_TEST_CASE(RadixSortingCustomType)
-{
-    static const size_t LARGE_VEC = 1000000;
+BOOST_AUTO_TEST_CASE(RadixSortingCustomType) {
+  static const size_t LARGE_VEC = 1000000;
 
-    typedef uint32_t UnsignedIntT;
+  typedef uint32_t UnsignedIntT;
 
-    std::vector<UnsignedIntT> elements;
-    sorting_practice::testing::createRandomVector<UnsignedIntT>(elements, 10000, LARGE_VEC);
+  std::vector<UnsignedIntT> elements;
+  sorting_practice::testing::createRandomVector<UnsignedIntT>(elements, 10000,
+                                                              LARGE_VEC);
 
-    std::vector<MyStruct> struct_elements;
-    struct_elements.reserve(elements.size());
-    for (size_t i = 0; i < elements.size(); ++i)
-    {
-        MyStruct mys; mys.key = elements[i];
+  std::vector<MyStruct> struct_elements;
+  struct_elements.reserve(elements.size());
+  for (size_t i = 0; i < elements.size(); ++i) {
+    MyStruct mys;
+    mys.key = elements[i];
 
-        struct_elements.push_back(mys);
-    }
+    struct_elements.push_back(mys);
+  }
 
-    MyStruct* start = &struct_elements.data()[0];
-    MyStruct* end   = start + struct_elements.size();
+  MyStruct* start = &struct_elements.data()[0];
+  MyStruct* end = start + struct_elements.size();
 
-    // radix
-    BOOST_REQUIRE(sorting_practice::testing::testPerformance(start, end,
-            RadixSortFunctorWrapper<MyStruct*>(),
-            "radix_sort"));
+  // radix
+  BOOST_REQUIRE(sorting_practice::testing::testPerformance(
+      start, end, RadixSortFunctorWrapper<MyStruct*>(), "radix_sort"));
 }
--- sorting/test/testing_common.hpp
+++ sorting/test/testing_common.hpp
@@ -12,32 +12,29 @@
 #include <iostream>
 #include <chrono>
 
-namespace sorting_practice
-{
+namespace sorting_practice {
 
-namespace testing
-{
+namespace testing {
 
-template<typename IntT>
-void createRandomVector(std::vector<IntT> &vec, IntT max_value, std::size_t max_size)
-{
-    std::random_device random_dev;
-    std::default_random_engine generator(random_dev());
-    generator.seed(random_dev());
-    std::uniform_int_distribution < IntT > distribution(0, max_value);
+template <typename IntT>
+void createRandomVector(std::vector<IntT> &vec, IntT max_value,
+                        std::size_t max_size) {
+  std::random_device random_dev;
+  std::default_random_engine generator(random_dev());
+  generator.seed(random_dev());
+  std::uniform_int_distribution<IntT> distribution(0, max_value);
 
-    vec.clear();
-    vec.reserve(max_size);
-    for (std::size_t i = 0; i < max_size; ++i)
-    {
-        vec.push_back(static_cast<IntT>(distribution(generator)));
-    }
+  vec.clear();
+  vec.reserve(max_size);
+  for (std::size_t i = 0; i < max_size; ++i) {
+    vec.push_back(static_cast<IntT>(distribution(generator)));
+  }
 }
 
 // Returns time difference in seconds
-double timeDiff(clock_t start, clock_t end)
-{
-    return (static_cast<double>(end) - static_cast<double>(start)) / static_cast<double>(CLOCKS_PER_SEC);
+double timeDiff(clock_t start, clock_t end) {
+  return (static_cast<double>(end) - static_cast<double>(start)) /
+         static_cast<double>(CLOCKS_PER_SEC);
 }
 
 /*
@@ -47,61 +44,64 @@ double timeDiff(clock_t start, clock_t end)
  *
  * Note: std::mismatch and std::equal use ''equality'' checks
  */
-template<typename IteratorT1, typename IteratorT2,
-    typename LessT = sorting_practice::Less<typename IteratorTraits<IteratorT1>::ValueType> >
-bool rangesEquivalent(IteratorT1 start, IteratorT1 end, IteratorT2 other, LessT const &cmp = LessT())
-{
-    IteratorT1 it = start;
-    IteratorT2 it_other = other;
-    for (; it != end; ++it, ++it_other)
-    {
-        if (cmp(*it, *it_other) || cmp(*it_other, *it))
-        {
-            return false;
-        }
+template <typename IteratorT1, typename IteratorT2,
+          typename LessT = sorting_practice::Less<
+              typename IteratorTraits<IteratorT1>::ValueType> >
+bool rangesEquivalent(IteratorT1 start, IteratorT1 end, IteratorT2 other,
+                      LessT const &cmp = LessT()) {
+  IteratorT1 it = start;
+  IteratorT2 it_other = other;
+  for (; it != end; ++it, ++it_other) {
+    if (cmp(*it, *it_other) || cmp(*it_other, *it)) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
 
 /*
  * Performance testing
  */
-template<typename IteratorT, typename SortFunctionT>
-bool testPerformance(IteratorT start, IteratorT end, SortFunctionT sort_fn, std::string const &sort_name)
-{
-    // Make a copy of the input elements, allows both algorithms to have same input
-    typedef typename sorting_practice::IteratorTraits<IteratorT>::ValueType ValueT;
-    std::vector<ValueT> range_copy;
-    range_copy.resize(end - start);
-    std::copy(start, end, range_copy.begin());
+template <typename IteratorT, typename SortFunctionT>
+bool testPerformance(IteratorT start, IteratorT end, SortFunctionT sort_fn,
+                     std::string const &sort_name) {
+  // Make a copy of the input elements, allows both algorithms to have same
+  // input
+  typedef typename sorting_practice::IteratorTraits<IteratorT>::ValueType
+      ValueT;
+  std::vector<ValueT> range_copy;
+  range_copy.resize(end - start);
+  std::copy(start, end, range_copy.begin());
 
-    // Custom sort
-    clock_t custom_start = clock();
-    sort_fn(start, end);
-    clock_t custom_stop = clock();
+  // Custom sort
+  clock_t custom_start = clock();
+  sort_fn(start, end);
+  clock_t custom_stop = clock();
 
-    // Standard sort
-    clock_t std_start = clock();
-    std::sort(range_copy.begin(), range_copy.end());
-    clock_t std_stop = clock();
+  // Standard sort
+  clock_t std_start = clock();
+  std::sort(range_copy.begin(), range_copy.end());
+  clock_t std_stop = clock();
 
-    bool rv = rangesEquivalent(start, end, range_copy.begin());
+  bool rv = rangesEquivalent(start, end, range_copy.begin());
 
-    if (!rv)
-    {
-        std::cerr << "Vector was not sorted!" << std::endl;
-    }
+  if (!rv) {
+    std::cerr << "Vector was not sorted!" << std::endl;
+  }
 
-    std::cout << "Time for " + sort_name + ": " << (timeDiff(custom_start, custom_stop) * 1.0e3) << "ms" << std::endl;
-    std::cout << "Time for std sort:  " << (timeDiff(std_start, std_stop) * 1.0e3) << "ms" << std::endl;
-    std::cout << "Number of elements: " << (end - start) << std::endl;
+  std::cout << "Time for " + sort_name + ": "
+            << (timeDiff(custom_start, custom_stop) * 1.0e3) << "ms"
+            << std::endl;
+  std::cout << "Time for std sort:  " << (timeDiff(std_start, std_stop) * 1.0e3)
+            << "ms" << std::endl;
+  std::cout << "Number of elements: " << (end - start) << std::endl;
 
-    return rv;
+  return rv;
 }
 
-} // testing
+}  // testing
 
-} // sorting_practice
+}  // sorting_practice
 
 #endif /* TESTING_COMMON_HPP_ */
